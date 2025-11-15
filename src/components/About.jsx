d900de897e903c64ff7989ab0fe6bbf42afad426
Nando <ernandofebrian6@gmail.com>
2025-11-16 01:00:42 +0700

    Finishin about section by making another components called AnimationTitle for the template title text for every section future

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react'
 import React from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
+import AnimationTitle from './AnimationTitle'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -28,10 +29,14 @@ const About = () => {
     return (
         <div id='about' className='min-h-screen w-screen'>
             <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-                <h2 className='font-family-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
-                <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-                    Disc<b>o</b>ver the world's<br /> l<b>a</b>rgest shared adventures
-                </div>
+                <h2 className='font-family-general text-sm uppercase md:text-[10px]'>
+                    Welcome to Zentry</h2>
+
+                <AnimationTitle 
+                    title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure" 
+                    containerClass='mt-5 !text-black text-center'
+                />
+
                 <div className='about-subtext'>
                     <p>The Game of Games begins-your life, now an epic MMORPG</p>
                     <p>Zentry unites every player from countless games and platforms</p>
@@ -40,7 +45,7 @@ const About = () => {
 
             <div className='h-dvh w-screen' id='clip'>
                 <div className='mask-clip-path about-image'>
-                    <img 
+                    <img
                         src='img/about.webp'
                         alt='Background'
                         className='absolute left-0 top-0 size-full object-cover'
